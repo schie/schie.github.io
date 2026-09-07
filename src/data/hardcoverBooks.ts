84da1f776a4575381ed8e1dac1fa96c2a029355d
schie <4561856+schie@users.noreply.github.com>
2026-09-07 00:30:49 +0000

    fix(reading): sync hardcover reading data

--- src/data/hardcoverBooks.ts
+++ src/data/hardcoverBooks.ts
@@ -23,18 +23,25 @@ export const currentlyReading: HardcoverBook[] = [
       "https://assets.hardcover.app/edition/30447628/23d6f2bd-115d-450f-a731-9fbcb59b04e8.jpg",
   },
   {
+    title: "Empire of the Vampire",
+    slug: "empire-of-the-vampire",
+    author: "Bon Orthwick, Jay Kristoff",
+    imageUrl:
+      "https://assets.hardcover.app/editions/30548430/7973802927976714.jpg",
+  },
+  {
+    title: "The Lost Metal",
+    slug: "the-lost-metal",
+    author: "Brandon Sanderson",
+    imageUrl:
+      "https://assets.hardcover.app/external_data/60987563/93e411dc55a6e9763635437407af2309d749fc4a.jpeg",
+  },
+  {
     title: "The Wild Robot Escapes",
     slug: "the-wild-robot-escapes",
     author: "Peter  Brown",
     imageUrl:
       "https://assets.hardcover.app/external_data/35422191/0fa8d7ceb502714596fbe26e4d6ae98d314e01fb.jpeg",
-  },
-  {
-    title: "Arcanum Unbounded: The Cosmere Collection",
-    slug: "arcanum-unbounded",
-    author: "Brandon Sanderson",
-    imageUrl:
-      "https://assets.hardcover.app/edition/31276301/b5b1c6b758306caf9553430c4ccf576f29b9bd19.jpeg",
   },
 ];
 
